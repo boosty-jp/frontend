13cb30f2a2b8b053438cd913dc58edcbe5e95522
tomokiya <t.elle.hormon@gmail.com>
2020-06-20 14:02:38 +0900

    fontawsomeが表示されない不具合修正

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,13 +2,16 @@ import wrapWithProvider from "./wrap-with-provider"
 import 'react-quill/dist/quill.snow.css'
 import 'easymde/dist/easymde.min.css'
 import 'antd/dist/antd.css';
-import 'styles/index.css'
 import 'highlight.js/styles/tomorrow-night-blue.css';
 import 'ant-design-pro/dist/ant-design-pro.css';
 import 'instantsearch.css/themes/reset.css';
 import 'tocbot/dist/tocbot.css';
 import '@fortawesome/fontawesome-free/js/fontawesome';
 import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/css/solid.min.css';
+import '@fortawesome/fontawesome-free/css/regular.min.css';
+import 'styles/index.css'
 
 export const wrapRootElement = wrapWithProvider;
 const ReactDOM = require('react-dom')
